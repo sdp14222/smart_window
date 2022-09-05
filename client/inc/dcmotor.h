8902a6cde50be95afdce198a96306d985958f29b
SangDon Park <sdp14222@gmail.com>
2022-09-05 12:38:16 +0900

    Modified dcmotor

---
+++ client/inc/dcmotor.h
@@ -0,0 +1,13 @@
+#ifndef __DCMOTOR_H__
+#define __DCMOTOR_H__
+
+#include <wiringPi.h>
+#include <softPwm.h>
+#include <stdio.h>
+#include <softPwm.h>
+#include <stdlib.h>
+
+void dcmotor_init(void);
+void dcmotor();
+
+#endif
--- client/src/dcmotor.c
+++ client/src/dcmotor.c
@@ -1,24 +1,20 @@
-#include <wiringPi.h>
-#include <stdio.h>
-#include <softPwm.h>
-#include <stdlib.h>
+#include "dcmotor.h"
 
 #define FOREWARD  13
 
-int main(void)
+void dcmotor_init(void)
 {
-	wiringPiSetupGpio();
 	pinMode(FOREWARD, OUTPUT);
 	softPwmCreate(FOREWARD, 0, 200);
+}
 
-	while(1) {
-		softPwmWrite(FOREWARD, 100);
-		printf("forward run \n");
+void dcmotor(void)
+{
+    softPwmWrite(FOREWARD, 100);
+    printf("forward run \n");
 
-		delay(1000);	
-		softPwmWrite(FOREWARD, 0);
-		printf("---------------\n");
-		delay(1000);	
-	}
-	return 0;
+    delay(1000);	
+    softPwmWrite(FOREWARD, 0);
+    printf("---------------\n");
+    delay(1000);	
 }
