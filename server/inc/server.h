6d7e05df864a2f9d5d20722461907d71d6e49008
SangDon Park <sdp14222@gmail.com>
2022-09-18 13:37:00 +0900

    Divide read_msg into functions, threads  On branch dev  Changes to be committed: 	modified:   Makefile 	modified:   inc/server.h 	modified:   src/server.c

--- server/inc/server.h
+++ server/inc/server.h
@@ -9,10 +9,22 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include "common.h"
 
 #define MSG_BUF_INIT_SIZE       10
-#define INIT_READ_BYTES         3
+#define INIT_READ_BYTES         7
 
-#define SERVER_PORT             39202
+struct init_msg_info
+{
+	int 			clnt_sock;
+	struct sockaddr_in	clnt_adr;
+	char			init_msg[MSG_BUF_INIT_SIZE];
+	uint8_t			cmd;
+	uint16_t		total_size;
+	uint32_t		uid;
+};
+
+void * handle_clnt(void *arg);
+void * read_msg(void *arg);
 
 #endif
--- server/src/server.c
+++ server/src/server.c
@@ -1,95 +1,177 @@
 /**************************************************
  * Created  : 2022.09.15
- * Modified : 2022.09.15
+ * Modified : 2022.09.17
  * Author   : SangDon Park
  **************************************************/
 #include "server.h"
 
-void * handle_clnt(void * arg);
 void send_msg(char * msg, int len);
 void error_handling(char * msg);
+static int rptos(struct init_msg_info *info);
 
 pthread_mutex_t mutx;
 
 int main(int argc, char *argv[])
 {
-	int serv_sock, clnt_sock;
-	struct sockaddr_in serv_adr, clnt_adr;
-	unsigned int clnt_adr_sz;
-	pthread_t t_id;
+	int serv_sock;
+	struct sockaddr_in serv_adr;
+	pthread_t read_thr;
 
 	pthread_mutex_init(&mutx, NULL);
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 
 	memset(&serv_adr, 0, sizeof(serv_adr));
 	serv_adr.sin_family 		= AF_INET; 
 	serv_adr.sin_addr.s_addr 	= htonl(INADDR_ANY);
-	serv_adr.sin_port 		= htons(SERVER_PORT);
+	serv_adr.sin_port 		= htons(atoi(SERVER_PORT));
 	
+	puts("server start...");
+
 	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
 		error_handling("bind() error");
 
 	if(listen(serv_sock, 5)==-1)
 		error_handling("listen() error");
-	
-	while(1)
-	{
-		clnt_adr_sz = sizeof(clnt_adr);
-		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
-		
-		pthread_mutex_lock(&mutx);
 
-		pthread_mutex_unlock(&mutx);
-	
-		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
-		pthread_detach(t_id);
+	pthread_create(&read_thr, NULL, read_msg, (void*)&serv_sock);
 
-		printf("Connected client IP  : %s \n", inet_ntoa(clnt_adr.sin_addr));
-		printf("Connected client Port: %d \n", ntohs(clnt_adr.sin_port));
-	}
+	pthread_join(read_thr, NULL);
+	
 	close(serv_sock);
 
 	return 0;
 }
+
+void * read_msg(void *arg)
+{
+	int serv_sock = *(int*)arg;
+	struct init_msg_info init_info;
+	int read_cnt, cnt;
+	unsigned int clnt_adr_sz = sizeof(init_info.clnt_adr);
+
+	while(1)
+	{
+		puts("client wait...");
+		init_info.clnt_sock = accept(serv_sock, (struct sockaddr*)&init_info.clnt_adr, &clnt_adr_sz);
+		cnt = read_cnt = 0;
+		memset(init_info.init_msg, 0, INIT_READ_BYTES);
+		
+		printf("Connected client IP  : %s \n", inet_ntoa(init_info.clnt_adr.sin_addr));
+		printf("Connected client Port: %d \n", ntohs(init_info.clnt_adr.sin_port));
+
+		while(read_cnt < INIT_READ_BYTES)
+		{
+			read_cnt = read(init_info.clnt_sock, init_info.init_msg, INIT_READ_BYTES);
+			cnt += read_cnt;
+			if(read_cnt == -1)
+			{
+				close(init_info.clnt_sock);
+				continue;
+			}
+		}
+		
+		init_info.cmd = *(uint8_t*)(init_info.init_msg);
+		printf("cmd : %d\n", init_info.cmd);
+		init_info.total_size = *(uint16_t*)(init_info.init_msg + 1);
+		printf("total_size : %d\n", init_info.total_size);
+		init_info.uid = *(uint32_t*)(init_info.init_msg + 3);
+		printf("uid : %d\n", init_info.uid);
+			
+		// 0000 0xxx
+		// 0000 0100
+		switch((init_info.cmd >> 2) & (uint8_t)0x1)
+		{
+			case 0:
+			// raspberry pi to server
+				puts("r.pi to server...");
+				if(!rptos(&init_info))
+				{
+					puts("rptos failed...");
+				}
+				break;
+			case 1:
+			// mobile to server
+#if 0
+				mtos();
+#endif
+				break;
+			default:
+				printf("init_info.cmd ^ (uint8_t)0x4 : %d\n", init_info.cmd ^ (uint8_t)0x4);
+				puts("abnormal connection...");
+		}
+		puts("client close...");
+	}
+}
+
+static int rptos(struct init_msg_info *info)
+{
+	pthread_t t_id;
+
+	// 0000 00xx
+	// 0000 0011
+	switch(info->cmd & ((uint8_t)0x3))
+	{
+		case 0:
+		// 00 : first connect signal
+			close(info->clnt_sock);
+			return 0;
+		case 1:
+		// confirm connection response to raspberry pi
+			close(info->clnt_sock);
+			return 0;
+		case 2:
+		// data from raspberry pi
+			puts("data from raspberry pi...");
+			pthread_create(&t_id, NULL, handle_clnt, (void*)info);
+			pthread_detach(t_id);
+			return 1;
+		default:
+			close(info->clnt_sock);
+			return 0;
+	}
+}
 	
 void * handle_clnt(void * arg)
 {
-	int clnt_sock = *((int*)arg);
-	
-	int msg_len= 0;
-	int total_size = MSG_BUF_INIT_SIZE;
-	char init_msg[MSG_BUF_INIT_SIZE] = {0};
-	char *rmsg = NULL;
-	int pos = INIT_READ_BYTES;
+	struct init_msg_info *info = (struct init_msg_info*)arg;
+	int msg_len, msg_len_sum= 0;
+	char *msg = NULL;
+	int msg_size = info->total_size - INIT_READ_BYTES;
 	int i;
-	
-	msg_len += read(clnt_sock, init_msg, INIT_READ_BYTES);
-	printf("msg_len : %d\n", msg_len);
-	total_size = *(uint16_t*)(init_msg + 1);
 
-	rmsg = (char*)malloc(total_size - INIT_READ_BYTES);
-	if(rmsg == NULL)
+	msg = (char*)malloc(msg_size);
+	if(msg == NULL)
 	{
-		printf("malloc error\n");
+		puts("handle_clnt malloc failed...");
 		return NULL;
+
 	}
 
-	while(msg_len < total_size)
+	while(msg_len_sum < msg_size)
 	{
-		msg_len += read(clnt_sock, rmsg, total_size);
-		printf("msg_len : %d\n", msg_len);
+		msg_len = read(info->clnt_sock, msg, info->total_size);
+		if(msg_len == -1)
+		{
+			puts("while read...clnt disconnect...");
+			close(info->clnt_sock);
+			return NULL;
+		}
+		else if(msg_len == 0)
+		{
+			puts("while read end");
+			break;
+		}
+		msg_len_sum += msg_len;
+		printf("msg_len_sum : %d\n", msg_len_sum);
 	}
 
-	printf("total_size : %d\n", total_size);
-
-	for(i = 0; i < total_size - INIT_READ_BYTES; i++)
+	for(i = 0; i < msg_size; i++)
 	{
-		printf("rmsg[%d] : %x\n", i + INIT_READ_BYTES, rmsg[i]);	
+		printf("msg[%d] : %x\n", i + INIT_READ_BYTES, msg[i]);	
 	}
 
-	free(rmsg);
-
-	close(clnt_sock);
+	free(msg);
+	close(info->clnt_sock);
 
 	return NULL;
 }
